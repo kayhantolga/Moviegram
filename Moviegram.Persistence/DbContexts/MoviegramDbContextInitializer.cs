466e66572ef4782efd9c7dbb9505d72cbd4b5a94
kayhantolga <kayhantolga@gmail.com>
2019-05-09 21:00:27 +0100

    Added fake data generation.

--- Moviegram.Persistence/DbContexts/MoviegramDbContextInitializer.cs
+++ Moviegram.Persistence/DbContexts/MoviegramDbContextInitializer.cs
@@ -1,4 +1,9 @@
-﻿namespace Moviegram.Persistence.DbContexts
+﻿using System;
+using System.Linq;
+using Bogus;
+using Moviegram.Domain.Entities;
+
+namespace Moviegram.Persistence.DbContexts
 {
     public static class MoviegramDbContextInitializer
     {
@@ -10,10 +15,52 @@
         private static void SeedEverything(MoviegramDbContext context)
         {
             context.Database.EnsureCreated();
-        }
 
-        private static void SeedMovies(MoviegramDbContext context)
-        {
+            //If db already initialized do not seed anything
+            if (context.Movies.Any()) return;
+
+            var testMovies = new Faker<Movie>()
+                .RuleFor(o => o.Id, f => Guid.NewGuid())
+                .RuleFor(o => o.Genre, f => f.Lorem.Paragraphs())
+                .RuleFor(o => o.Title, f => $"{f.Hacker.Noun()} {f.Hacker.Verb()}");
+
+            var testCelebrities = new Faker<Celebrity>()
+                .RuleFor(u => u.Id, f => Guid.NewGuid())
+                .RuleFor(u => u.Name, f => $"{f.Name.FirstName()} {f.Name.LastName()}")
+                .RuleFor(u => u.ProfilePhoto, f => f.Internet.Avatar());
+
+            var celebrities = testCelebrities.Generate(100);
+            context.Celebrities.AddRange(celebrities);
+            var movies = testMovies.Generate(100);
+
+            var random = new Random();
+            foreach (var movie in movies)
+            {
+                //Take max 10 random actor and make a movie
+                var actorCount = random.Next(10);
+                var tempActors = celebrities.OrderBy(r => Guid.NewGuid()).Take(actorCount).ToList();
+                foreach (var celebrity in tempActors)
+                    movie.Actors.Add(new MovieCelebrity
+                    {
+                        CelebrityId = celebrity.Id,
+                        MovieId = movie.Id
+                    });
+
+                var timeCount = random.Next(5);
+                var startTime = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, 9, 0, 0);
+                //Generate average movie time
+                var averageTime = (double)9 / timeCount;
+
+                for (var i = 0; i < timeCount; i++)
+                    movie.MovieShowTimes.Add(new MovieShowTime
+                    {
+                        Id = Guid.NewGuid(),
+                        ShowTimeStart = startTime.AddMinutes(averageTime * 60 * i + 30),
+                        ShowTimeEnd = startTime.AddMinutes(averageTime * 60 * (i + 1))
+                    });
+                context.Movies.Add(movie);
+            }
+
             context.SaveChanges();
         }
     }
--- Moviegram/Program.cs
+++ Moviegram/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Moviegram.Persistence.DbContexts;
 
 namespace Moviegram
 {
@@ -14,7 +17,28 @@ namespace Moviegram
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<MoviegramDbContext>();
+
+                    context.Database.Migrate();
+                    MoviegramDbContextInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
