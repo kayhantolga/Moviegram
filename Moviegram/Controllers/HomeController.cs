19257d4624918e6215c21228e15ba7cc4232d0c3
kayhantolga <kayhantolga@gmail.com>
2019-05-10 21:41:11 +0100

    added ping pong service

---
+++ Moviegram/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moviegram.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        ///     Check is service alive
+        /// </summary>
+        /// <returns>pong</returns>
+        [HttpGet]
+        [Route("ping")]
+        [ProducesResponseType(typeof(string), 200)]
+        public async Task<IActionResult> GetDetailInformation()
+        {
+            return Ok("pong");
+        }
+    }
+}
